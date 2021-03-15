5a4282c7b0521f0ee9fd73752ab943ab807d2715
panos <gpawnrt13@gmail.com>
2021-03-15 10:31:33 -0700

    add master_numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,5 +13,10 @@ public class Main {
         System.out.println("master_branch");
         System.out.println("branch_new");
 
+        System.out.println("master1");
+        System.out.println("master3");
+        System.out.println("master2");
+        System.out.println("master4");
+
     }
 }
